183fb89efe80ad004b934d65805acca36be5ee9a
Jorge García <jorgemgr94@gmail.com>
2026-05-02 22:05:02 -0600

    feat: implement environment variable management using Pydantic Settings and migrate configuration to env files

---
+++ config/env.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class EnvironmentSettings(BaseSettings):
+    DJANGO_SETTINGS_MODULE: str = "config.settings"
+    DEBUG: bool = False
+    SECRET_KEY: str
+    ALLOWED_HOSTS: list[str] = ["localhost", "127.0.0.1"]
+    
+    DB_NAME: str = "pets_db"
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "postgres"
+    DB_HOST: str = "localhost"
+    DB_PORT: str = "5432"
+    
+    REDIS_URL: str = "redis://localhost:6379/0"
+
+    # Lee automáticamente desde el archivo .env
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+
+
+env = EnvironmentSettings()
--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from config.env import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*f#a4hx(#v+u^q#6#1suiz^3yzzqxy(8ius)52ggyw(0^=50sk"
+SECRET_KEY = env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.DEBUG
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS: list[str] = env.ALLOWED_HOSTS
 
 
 # Application definition
@@ -74,10 +76,17 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.DB_NAME,
+        "USER": env.DB_USER,
+        "PASSWORD": env.DB_PASSWORD,
+        "HOST": env.DB_HOST,
+        "PORT": env.DB_PORT,
     }
 }
+
+# Celery
+CELERY_BROKER_URL = env.REDIS_URL
 
 
 # Password validation
