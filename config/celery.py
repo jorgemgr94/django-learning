b8dd5b2df35d2c1488aa870fbd3d1ae4f03d2bcd
Jorge García <jorgemgr94@gmail.com>
2026-05-02 23:39:19 -0600

    feat: update celery configuration comments

--- config/celery.py
+++ config/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery("conectapelu2")
-# Lee config desde Django settings con prefijo CELERY_
+# Read config from Django settings with CELERY_ prefix
 app.config_from_object("django.conf:settings", namespace="CELERY")
-# Auto-descubre tasks en INSTALLED_APPS
+# Auto-discover tasks in INSTALLED_APPS
 app.autodiscover_tasks()
